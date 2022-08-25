2d2a68be173b7bd2971232681a93b6cfaaa21637
Xerethars <mkowalski1999@gmail.com>
2022-08-25 13:58:54 +0200

    CHORE: Creating empty reusable components

---
+++ src/app/components/general/pokemon-wrapper/pokemon-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-wrapper',
+  templateUrl: './pokemon-wrapper.component.html',
+  styleUrls: ['./pokemon-wrapper.component.css']
+})
+export class PokemonWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
