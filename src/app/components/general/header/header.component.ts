264c4b96847788d4af258b046164a32d0964ce42
Xerethars <mkowalski1999@gmail.com>
2022-09-02 10:19:14 +0200

    STYLE: adding header styling and showing page titles as well as username if user is logged in

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  public getPath() {
+    const path = this.router.url;
+
+    return path.substring(1).toUpperCase();
+  }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  get username(): string | null {
+    return this.authService.getUsername();
   }
 
 }
