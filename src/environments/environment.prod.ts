3e959ad3fb20bd4f9e5f8f0a0878ae4662c4a6b5
Michal Kowalski <mkowalski1999@gmail.com>
2022-09-04 21:46:58 +0200

    adding production = true field to envirnoment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   trainerAPI: "https://noroff-assignment-api-michalk.herokuapp.com/trainers",
   pokemonAPI: "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0",
   APIKey: "123321",
